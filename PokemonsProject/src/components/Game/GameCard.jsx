77edf0e441c8dcdb92b1638a561e7f7d4dd08df6
Majoodeh <Majdhussein144@gmail.com>
2026-01-11 15:19:12 +0100

    Save, Working on GameCard

---
+++ PokemonsProject/src/components/Game/GameCard.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import ReactCardFlip from "react-card-flip";
+import pokeballImg from "../../imgs/pokeball.png";
+import Navbar from "../Navbar/Navbar.jsx";
+
+function GameCard(props) {
+  const { name, imageUrl, id, isFlipped, flipCard } = props;
+  return (
+    <div
+      className="flex justify-center items-center h-60 w-60"
+      name={name}
+      key={id}
+      onClick={flipCard}
+    >
+      <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+        <div
+          key="front"
+          className="relative w-60 h-60 min-w-20 min-h-20 max-w-80 max-h-80 rounded-lg overflow-hidden border border-gray-200 shadow-sm bg-white"
+        >
+          {/* THE IMAGE:  */}
+          <img
+            src={pokeballImg} //! needs to be changed to prop later
+            alt="Small card"
+            className="absolute inset-0 w-full h-full object-cover"
+          />
+        </div>
+
+        {/* back side of the card */}
+
+        <div
+          key="back"
+          className="relative w-60 h-60 min-w-20 min-h-20 max-w-80 max-h-80 rounded-lg overflow-hidden border border-gray-200 shadow-sm bg-white"
+          onClick={() => setIsFlipped(false)}
+        >
+          {/* THE IMAGE:  */}
+          <img
+            src={imageUrl}
+            alt="Small card"
+            className="absolute inset-0 w-full h-full object-cover"
+          />
+
+          {/* THE SHADOW */}
+          <div
+            className="absolute inset-0 pointer-events-none"
+            style={{
+              background: "radial-gradient(black, transparent 60%)",
+              transform: "rotate3d(0, 0, 1, 20deg) scale3d(1, 0.6, 1)",
+              opacity: 0.1,
+            }}
+          ></div>
+        </div>
+      </ReactCardFlip>
+    </div>
+  );
+}
+
+export default GameCard;
